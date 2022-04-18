f8c0deca7f17c89c2e756da4b206d2287a46423e
Sibi Prabakaran <sibi@psibi.in>
2022-04-18 13:11:20 +0530

    Amber installation works in all the three platforms now

---
+++ README.md
@@ -0,0 +1,39 @@
+# setup-amber
+
+[![Github CI](https://github.com/psibi/setup-amber/actions/workflows/test.yml/badge.svg)](https://github.com/psibi/setup-amber/actions/workflows/test.yml)
+
+Github action to install and cache the [amber](https://github.com/fpco/amber) tool.
+
+# Usage
+
+In most cases all you need is the following in your workflow:
+
+``` yaml
+- uses: psibi/setup-amber@v1
+```
+
+If you want a specific version of ~amber~, you can specify this by
+passing the ~amber-version~ input:
+
+``` yaml
+- uses: psibi/setup-amber@v1
+  with:
+    amber-version: 'v0.1.3'
+```
+
+In rare circumstances you might get rate limiting errors, this is
+because this workflow has to make requests to GitHub API in order to
+list available releases. If this happens you can set the `GITHUB_TOKEN`
+environment variable.
+
+``` yaml
+- uses: psibi/setup-amber@v1
+  env:
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+### Inputs
+
+| Name           | Required | Description                             | Type   | Default |
+| -------------- | -------- | --------------------------------------- | ------ | ------- |
+| `amber-version` | no       | A valid NPM-style semver specification. | string | *       |
--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,37 +4,36 @@ import {
   OS,
   associateOs,
   getAmberForMachine,
-  Release,
-} from "../src/main";
-import * as process from "process";
-import * as cp from "child_process";
-import * as path from "path";
-import { expect, test } from "@jest/globals";
+  Release
+} from '../src/main'
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
+import {expect, test} from '@jest/globals'
 
-test("three assets present for amber v0.1.3 version", async () => {
-  const release = await getRelease("v0.1.3");
-  console.log("release", release);
-  expect(release.length).toBe(3);
-});
+test('three assets present for amber v0.1.3 version', async () => {
+  const release = await getRelease('v0.1.3')
+  expect(release.length).toBe(3)
+})
 
-test("no assets available for invalid version", async () => {
-  const release = await getRelease("non_existent");
-  expect(release.length).toBe(0);
-});
+test('no assets available for invalid version', async () => {
+  const release = await getRelease('non_existent')
+  expect(release.length).toBe(0)
+})
 
-test("latest release contains three assets", async () => {
-  const release = await getLatestRelease();
-  expect(release.length).toBe(3);
-});
+test('latest release contains three assets', async () => {
+  const release = await getLatestRelease()
+  expect(release.length).toBe(3)
+})
 
-test("maps OS properly", async () => {
-  expect(associateOs("amber-x86_64-unknown-linux-musl")).toBe(OS.Linux);
-  expect(associateOs("amber-x86_64-pc-windows-gnu.exe")).toBe(OS.Windows);
-  expect(associateOs("amber-x86_64-apple-darwin")).toBe(OS.Mac);
-});
+test('maps OS properly', async () => {
+  expect(associateOs('amber-x86_64-unknown-linux-musl')).toBe(OS.Linux)
+  expect(associateOs('amber-x86_64-pc-windows-gnu.exe')).toBe(OS.Windows)
+  expect(associateOs('amber-x86_64-apple-darwin')).toBe(OS.Mac)
+})
 
-test("proper filtering happening for Linux", async () => {
-  const releases = await getRelease("v0.1.3");
-  const release = getAmberForMachine(releases);
-  expect(release!.os).toBe(OS.Linux);
-});
+test('proper filtering happening for Linux', async () => {
+  const releases = await getRelease('v0.1.3')
+  const release = getAmberForMachine(releases)
+  expect(release!.os).toBe(OS.Linux)
+})
